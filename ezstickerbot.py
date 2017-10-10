5358aead706c7a6b5ff900f1b982e9ceeac38fe7
BasedComrade <basedcomrade@gmail.com>
2017-10-10 00:21:49 -0400

    Initial release

---
+++ ezstickerbot.py
@@ -0,0 +1,73 @@
+import logging
+import os
+import time
+
+from PIL import Image
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
+
+# setup logger
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def start(bot, update):
+    pass
+
+
+def main():
+    updater = Updater("350549033:AAFNmoBp1F95NIrFc5ISo5nT_vSGmjJdj4A")
+    dispatcher = updater.dispatcher
+
+    # register start command handler
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(MessageHandler(Filters.photo, image_received))
+
+    # register error handler
+    dispatcher.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+def image_received(bot, update):
+    # feedback to show bot is processing
+    time.sleep(.5)
+    bot.send_chat_action(chat_id=update.message.chat_id, action='upload_photo')
+    photo_id = update.message.photo[-1].file_id
+
+    # download file
+    file = bot.get_file(file_id=photo_id)
+    ext = '.' + file.file_path.split('/')[-1].split('.')[1]
+    download_path = photo_id + ext
+    file.download(custom_path=download_path)
+
+    # process image
+    image = Image.open(download_path)
+    width, height = image.size
+    reference_length = max(width, height)
+    ratio = 512 / reference_length
+    new_width = int(width * ratio)
+    new_height = int(height * ratio)
+    image = image.resize((new_width, new_height), Image.ANTIALIAS)
+    formatted_path = photo_id + '_formatted.png'
+    image.save(formatted_path)
+
+    # send formated image as a document
+    document = open(formatted_path, 'rb')
+    bot.send_document(chat_id=update.message.chat_id, document=document, filename='sticker.png',
+                      caption='Forward this to @Stickers')
+
+    # delete local files
+    os.remove(download_path)
+    os.remove(formatted_path)
+
+
+# logs bot errors thrown
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"' % (update, error))
+
+
+if __name__ == '__main__':
+    main()
