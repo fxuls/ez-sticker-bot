29a7c9effe8309d8abb6503c7a9966fd54ae09d1
fxuls <32501694+fxuls@users.noreply.github.com>
2020-05-06 14:58:00 -0400

    Update donate command message Make payment options not be part of translation string.

--- ezstickerbot.py
+++ ezstickerbot.py
@@ -562,8 +562,8 @@ def change_lang_command(update: Update, context: CallbackContext):
 @run_async
 def donate_command(update: Update, context: CallbackContext):
     message = update.message
-    message_text = get_message(message.chat_id, "donate").format(config['donate_paypal'], config['donate_btc'],
-                                                                 config['donate_eth'])
+    message_text = get_message(message.chat_id, "donate") + "\n\n*Paypal:* {}\n*BTC:* `{}`\n*ETH:* `{}`"\
+        .format(config['donate_paypal'], config['donate_btc'], config['donate_eth'])
     message.reply_markdown(message_text, disable_web_page_preview=True)
 
 
